dee8f01bc5207181fdec0cc6eefdbc9ea8fcb92a
wwwhoami <subman450@gmail.com>
2025-04-12 21:58:52 +0300

    feat(server): implement TCP listener for incoming connections

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
-#![allow(unused_imports)]
 use std::net::TcpListener;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    let listener = TcpListener::bind("127.0.0.1:9092").expect("unable to bind to port");
 
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!(
+                    "accepted new connection from {}",
+                    stream.peer_addr().unwrap()
+                );
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
